968edb058305290ec18555347b6ac0cdbce54e5e
fencer-yd <982039620@qq.com>
2019-04-03 23:20:36 +0800

    update request

--- src/pages/main/root_manage/index.vue
+++ src/pages/main/root_manage/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="rm-main">
     <div class="rm-title">
-      <span>权限管理</span>
+      <span>用户管理</span>
     </div>
     <div class="rm-body">
       <el-table
@@ -13,31 +13,42 @@
         </el-table-column>
         <el-table-column
           prop="name"
-          label="用户名">
+          label="用户名"
+          width="180">
         </el-table-column>
         <el-table-column
           prop="phone"
+          width="120"
           label="手机">
         </el-table-column>
         <el-table-column
           prop="username"
+          width="120"
           label="登录名">
         </el-table-column>
         <el-table-column
-          label="最后登录时间">
+          label="创建时间">
           <template slot-scope="scope">
             <div>
-              {{utils.momentDate((scope.row.last_login_time ? Number(scope.row.last_login_time) : null), 'date_time')}}
+              {{utils.momentDate((scope.row.create_time ? Number(scope.row.create_time) : null), 'date_time')}}
             </div>
           </template>
         </el-table-column>
         <el-table-column
           label="角色">
           <template slot-scope="scope">
-            <div>
-              <el-select :disabled="userData.id === scope.row.id" @change="changeSelect(scope.row)" v-model="scope.row.role">
-                <el-option v-for="(item, index) in roleList" :key="index" :value="item.id" :label="item.name"></el-option>
-              </el-select>
+            <div class="tag-box">
+              <el-tag v-show="scope.row.role_name_list" v-for="(item, index) in scope.row.role_name_list" :key="index">{{item}}</el-tag>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column
+          width="100"
+          label="操作">
+          <template slot-scope="scope">
+            <div class="op-box">
+              <el-button v-show="!scope.row.role_name_list" @click="setRole($event, scope.row)" type="text">设置角色</el-button>
+              <el-button v-show="scope.row.role_name_list" @click="editRole($event, scope.row)" type="text">编辑</el-button>
             </div>
           </template>
         </el-table-column>
@@ -54,6 +65,11 @@
         :total="count">
       </el-pagination>
     </div>
+    <edit-role
+      v-model="modals.editRole"
+      @close="closeEditRole"
+      @finish="finshEditRole">
+    </edit-role>
   </div>
 </template>
 <script>
@@ -68,6 +84,9 @@ export default {
         search_content: '',
         page_no: 1,
         page_size: 20
+      },
+      modals: {
+        editRole: false
       }
     }
   },
@@ -108,6 +127,45 @@ export default {
           this.$message.success('修改成功')
         } else {
           this.$message.error(res.msg || '修改失败')
+        }
+      })
+    },
+    setRole (e, item) {
+      e.stopPropagation()
+      this.$store.commit('resetUserRoleCache')
+      this.$store.commit('setUserCache', item)
+      this.modals.editRole = true
+    },
+    editRole (e, item) {
+      e.stopPropagation()
+      this.$store.dispatch('getRoleByUser', {
+        user_id: item.id
+      }).then(res => {
+        if (res) {
+          this.$store.commit('setUserCache', item)
+          this.modals.editRole = true
+        }
+      })
+    },
+    closeEditRole () {
+      this.$store.commit('resetUserDataCache')
+      this.$store.commit('resetUserRoleCache')
+      this.modals.editRole = false
+    },
+    finshEditRole (data) {
+      const putData = {
+        user_id: this.$store.state.user.userCache && this.$store.state.user.userCache.id,
+        role_id_list: data,
+        type: 1
+      }
+
+      this.userService.saveAllotUser(putData).then(res => {
+        if (res.status === 0) {
+          this.$message.success('保存成功')
+          this.modals.editRole = false
+          this.$store.commit('resetUserDataCache')
+          this.$store.commit('resetUserRoleCache')
+          this.search()
         }
       })
     }
