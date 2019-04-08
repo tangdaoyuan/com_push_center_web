edeae6c8e8823fcae679f6ac93b0f50254faf03b
tangdaoyuan <1197633750@qq.com>
2019-04-08 11:26:16 +0800

    restructe service

--- src/service/nw_service.js
+++ src/service/nw_service.js
@@ -1,33 +1,33 @@
 import Service from './index.js'
 
-class NwService {
+class NwService extends Service {
   getWatchData (data) {
     return new Promise((resolve) => {
-      Service.TEMPLATE_GET('/api/task/monitor/list', data, resolve)
+      this.TEMPLATE_GET('/api/task/monitor/list', data, resolve)
     })
   }
 
   watchStatistics (data) {
     return new Promise((resolve) => {
-      Service.TEMPLATE_GET('/api/task/monitor/statistics', data, resolve)
+      this.TEMPLATE_GET('/api/task/monitor/statistics', data, resolve)
     })
   }
 
   matchList (data) {
     return new Promise((resolve) => {
-      Service.TEMPLATE_GET('/api/task/data_list', data, resolve)
+      this.TEMPLATE_GET('/api/task/data_list', data, resolve)
     })
   }
 
   pushList (data) {
     return new Promise((resolve) => {
-      Service.TEMPLATE_GET('/api/task/push_list', data, resolve)
+      this.TEMPLATE_GET('/api/task/push_list', data, resolve)
     })
   }
 
   changeStatus (data) {
     return new Promise((resolve) => {
-      Service.TEMPLATE_PUT('/api/task/switch', data, resolve)
+      this.TEMPLATE_PUT('/api/task/switch', data, resolve)
     })
   }
 }
