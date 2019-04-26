87eab15d29598c1c371b033f13f7340152483cdf
tangdaoyuan <1197633750@qq.com>
2019-04-26 17:11:07 +0800

    add: static page 'api and database flow-data-config'

---
+++ src/modals/task_step/taskStepSecondFsys.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="step-body" v-show="step === 1">
+    <div class="flow-sys-main">
+      <div class="sys-header">
+        <span>连接配置</span>
+        <span>推送数据库是默认实时推送</span>
+      </div>
+      <div class="grid-main sys-main">
+        <div class="item r-span">
+          <i class="red-dot"></i>
+          <span>数据库地址</span>
+        </div>
+        <div class="item">
+          <i class="red-dot"></i>
+          <input type="text">
+        </div>
+        <div class="item r-span">
+          <i class="red-dot"></i>
+          <span>数据表</span>
+        </div>
+        <div class="item">
+          <input type="text">
+        </div>
+        <div class="item r-span">
+          <i class="red-dot"></i>
+          <span>数据库端口</span>
+        </div>
+        <div class="item">
+          <input type="text">
+        </div>
+        <div class="item r-span">
+          <span>起止时间</span>
+        </div>
+        <div class="item">
+          <input type="text">
+        </div>
+        <div class="item r-span">
+          <i class="red-dot"></i>
+          <span>用户名</span>
+        </div>
+        <div class="item user-input">
+          <input type="text">
+        </div>
+        <div class="item r-span pwd-label">
+          <i class="red-dot"></i>
+          <span>密码</span>
+        </div>
+        <div class="item pwd-input">
+          <input type="text">
+        </div>
+        <div class="item r-span">
+          <i class="red-dot"></i>
+          <span>数据库名</span>
+        </div>
+        <div class="item">
+          <input type="text">
+        </div>
+      </div>
+    </div>
+    <div class="step-footer">
+      <el-button v-show="!$store.state.task.taskData" @click="prev()">上一步</el-button>
+      <el-button type="primary" @click="next()">{{$store.state.task.taskData ? '完成修改' : '下一步'}}</el-button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    step: Number
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    prev () {
+      this.$emit('prev')
+    },
+    next () {
+      this.$emit('next')
+    }
+  },
+  watch: {
+    step () {
+      if (this.step === -1) {
+        Object.assign(this.$data, this.$options.data())
+      }
+    }
+  }
+}
+</script>
--- src/modals/tc_modals/taskManage.vue
+++ src/modals/tc_modals/taskManage.vue
@@ -12,20 +12,27 @@
       <div class="step-box" v-show="currentStep > 0">
         <Steps :current="currentStep">
           <!-- <Step title="创建基本信息" @click.native="editStep($event, 0)" content=""></Step> -->
-          <Step title="设置任务字段" v-show="true" @click.native="editStep($event, 1)" content=""></Step>
           <Step title="选择工作表" v-show="false" @click.native="editStep($event, 1)" content=""></Step>
+          <Step title="设置任务字段" v-show="true" @click.native="editStep($event, 1)" content=""></Step>
           <Step title="设置推送信息" @click.native="editStep($event, 2)" content=""></Step>
           <Step title="设置推送用户" @click.native="editStep($event, 3)" content=""></Step>
           <Step title="设置推送通道" @click.native="editStep($event, 4)" content=""></Step>
         </Steps>
       </div>
-      <!-- <task-step-second @refresh="refresh" :step="currentStep" @next="next" @prev="prev"/> -->
-      <task-step-second-flow @refresh="refresh" :step="currentStep" @next="next" @prev="prev"/>
+      <task-step-second @refresh="refresh" :step="currentStep" @next="next" @prev="prev"/>
+      <!-- <task-step-second-fuser  v-show="false" @refresh="refresh" :step="currentStep" @next="next" @prev="prev"/> -->
+      <!-- <task-step-second-fsys v-show="false" @refresh="refresh" :step="currentStep" @next="next" @prev="prev"/> -->
+      <!-- <task-step-second-fapi v-show="false" @refresh="refresh" :step="currentStep" @next="next" @prev="prev"/> -->
       <task-step-third @refresh="refresh" :step="currentStep" @next="next" @prev="prev"/>
       <task-step-forth @refresh="refresh" :step="currentStep" @next="next" @prev="prev"/>
       <task-step-fifth @refresh="refresh" :step="currentStep" @finish="finish" @prev="prev"/>
     </div>
-    <div class="hide" slot="footer"></div>
+    <div class="task-footer" v-show="false" slot="footer">
+      <div class="footer">
+        <el-button v-show="!$store.state.task.taskData" @click="prev()">上一步</el-button>
+        <el-button type="primary" @click="next()">{{$store.state.task.taskData ? '完成修改' : '下一步'}}</el-button>
+      </div>
+    </div>
   </Modal>
 </template>
 <script>
