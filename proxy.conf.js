fc68bed002456d8aa15f360710a52ec9e4f615be
tangdaoyuan <1197633750@qq.com>
2019-07-16 10:54:15 +0800

    Update proxy.conf.js

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,16 +1,6 @@
 module.exports = {
   '/api': {
-    // 'target': 'http://119.3.227.236:8080',
-
-    // 'target': 'http://192.168.1.93:8081',
-    // 'target': 'http://119.3.227.236:8080',
-    // 'target': 'http://119.3.227.236:8081',
-    // 'target': 'http://43.241.237.154:8080',
-    // 'target': 'http://192.168.18.245:8081',
-    // 'target': 'http://192.168.18.231:8081', // 丁丁
-    'target': 'http://119.3.227.236:8080', // 丁丁 ga-haizhi-5g
-    // 'target': 'http://192.168.0.47:8080', // 陈波 mifi-8464
-    // 'target': 'http://192.168.1.175:8080', // 陈波 ga-haizhi
+    'target': 'ip addr'
     'changeOrigin': true
   }
 }
